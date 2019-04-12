83bee49a6783acb1df8ba42225c9b76b70ae70a9
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2019-04-12 17:08:51 +0200

    Adding test :)

--- test/sorted_chips_row_test.dart
+++ test/sorted_chips_row_test.dart
@@ -1,13 +1,104 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:sorted_chips_row/sorted_chips_row.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  group('SortedChipsRow', () {
+    testWidgets("doesn\'t render Chips if passed empty list",
+        (WidgetTester wt) async {
+      await wt.pumpWidget(MaterialApp(home: SortedChipsRow(chips: [])));
+      expect(find.byType(Chip), findsNothing);
+    });
+
+    testWidgets("renders single Chip with given label",
+        (WidgetTester wt) async {
+      final Text label = Text('Test Label');
+      await wt.pumpWidget(
+          MaterialApp(home: SortedChipsRow(chips: [ChipSpec(label: label)])));
+
+      expect(
+          find.byWidgetPredicate(
+              (widget) => widget is Chip && widget.label == label),
+          findsOneWidget);
+    });
+
+    testWidgets("renders single Chip with other attributes",
+        (WidgetTester wt) async {
+      final ChipSpec spec = ChipSpec(
+          label: Text('Foo bar'),
+          labelStyle: TextStyle(color: Colors.amber),
+          avatar: CircleAvatar(child: Text('F')),
+          clipBehaviour: Clip.hardEdge,
+          enabledColor: Colors.white,
+          disabledColor: Colors.black,
+          elevation: 100.0,
+          initiallyEnabled: true,
+          key: Key("Chip Key"));
+
+      await wt.pumpWidget(MaterialApp(home: SortedChipsRow(chips: [spec])));
+
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is Chip &&
+              widget.label == spec.label &&
+              widget.labelStyle == spec.labelStyle &&
+              widget.avatar == spec.avatar &&
+              widget.clipBehavior == spec.clipBehaviour &&
+              widget.backgroundColor == spec.enabledColor &&
+              widget.elevation == spec.elevation),
+          findsOneWidget);
+    });
+
+    testWidgets("Rerenders chips according to default onPress function",
+        (WidgetTester wt) async {
+      final ChipSpec spec1 = ChipSpec(
+          label: Text('Foo Bar'),
+          initiallyEnabled: false,
+          enabledColor: Colors.white,
+          disabledColor: Colors.black);
+      final ChipSpec spec2 = ChipSpec(
+          label: Text('Bar Baz'),
+          initiallyEnabled: true,
+          enabledColor: Colors.white,
+          disabledColor: Colors.black);
+
+      await wt.pumpWidget(MaterialApp(
+          home: Center(child: SortedChipsRow(chips: [spec1, spec2]))));
+      await wt.pumpAndSettle();
+
+      expect(find.byType(Chip), findsNWidgets(2));
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is Chip &&
+              widget.label == spec1.label &&
+              widget.backgroundColor == Colors.black),
+          findsOneWidget);
+
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is Chip &&
+              widget.label == spec2.label &&
+              widget.backgroundColor == Colors.white),
+          findsOneWidget);
+
+      await wt.tap(find.byType(Chip).first);
+      await wt.tap(find.byType(Chip).last);
+      await wt.pumpAndSettle();
+
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is Chip &&
+              widget.label == spec1.label &&
+              widget.backgroundColor == Colors.white),
+          findsOneWidget);
+      expect(
+          find.byWidgetPredicate((widget) =>
+              widget is Chip &&
+              widget.label == spec2.label &&
+              widget.backgroundColor == Colors.black),
+          findsOneWidget);
+    });
   });
 }
